bbee286fd2adee54c0b16dbf30051d5369b4c8ef
Seljehaug <cs_seljehaug@hotmail.com>
2017-05-22 15:23:54 +0200

    bootstrap

--- index.php
+++ index.php
@@ -1,21 +1,42 @@
-<?php
-	require_once("connect.php");
-	$query = 'CREATE TABLE IF NOT EXISTS test (
-		id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-		test VARCHAR(100))';
-	if ($db->exec($query)===false){
-		die('Query failed(4):' . $db->errorInfo()[2]);
-	}
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Index</title>
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+      <link rel='stylesheet' type='text/css' href='css/styles.css'>
+   </head>
 
-	$sql = "SELECT * FROM test";
-	$stmnt = $db->prepare($sql);
-	if (!$stmnt->execute(array()))
-		die('Query failed:' . $db->errorInfo()[2]);
+	<?php
+		require_once("connect.php");
+		$query = 'CREATE TABLE IF NOT EXISTS test (
+			id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+			test VARCHAR(100))';
+		if ($db->exec($query)===false){
+			die('Query failed(4):' . $db->errorInfo()[2]);
+		}
 
-	$result = $stmnt->fetchAll(PDO::FETCH_OBJ);
+		$sql = "SELECT * FROM test";
+		$stmnt = $db->prepare($sql);
+		if (!$stmnt->execute(array()))
+			die('Query failed:' . $db->errorInfo()[2]);
 
-	foreach ($result as $r) {
-		echo $r->test;
-	}
+		$result = $stmnt->fetchAll(PDO::FETCH_OBJ);
 
-?>
+		foreach ($result as $r) {
+			echo $r->test;
+		}
+
+	?>
+	<body id="index">
+      <div class="container">
+			<div class="col-md-6 col-md-offset-3">
+				<p>Content here</p>
+			</div>
+		</container>
+	</body>
+</html>
