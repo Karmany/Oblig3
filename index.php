8cb5f212084f6e68c3b2c699869a9b0fed988b4e
Seljehaug <cs_seljehaug@hotmail.com>
2017-05-22 15:54:15 +0200

    ye

--- index.php
+++ index.php
@@ -33,8 +33,8 @@
 
 	?>
 	<body id="index">
-      <div class="container">
-			<div class="col-md-6 col-md-offset-3">
+      <div class="container-fluid">
+			<div class="col-md-10 col-md-offset-1">
 				<p>Content here</p>
 			</div>
 		</container>
