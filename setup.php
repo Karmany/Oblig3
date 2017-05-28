bd43c12bf76a42fe632b15bc1c15d238dd1f23d3
Håkon Horvei <haakonhorvei@hotmail.com>
2017-05-28 15:10:09 +0200

    SETUP uten data

--- setup.php
+++ setup.php
@@ -28,6 +28,7 @@ if ($db->exec($query)===false){
 	die('Query failed(3):' . $db->errorInfo()[2]);
 }
 
+
 // Create table for users
 $query = 'CREATE TABLE IF NOT EXISTS users (
 	userID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -38,4 +39,70 @@ $query = 'CREATE TABLE IF NOT EXISTS users (
 	password VARCHAR(256))';
 if ($db->exec($query)===false){
 	die('Query failed(4):' . $db->errorInfo()[2]);
+}
+
+// Create table for items
+$query = 'CREATE TABLE IF NOT EXISTS items (
+	itemID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(128) NOT NULL,
+	description VARCHAR(256) NOT NULL,
+	userID INT NOT NULL,
+	mapLong INT NOT NULL,
+	mapLat INT NOT NULL,
+	date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+	FOREIGN KEY (userID) REFERENCES users(userID))';
+if ($db->exec($query)===false){
+	die('Query failed(5):' . $db->errorInfo()[2]);
+}
+
+// Create table for category
+$query = 'CREATE TABLE IF NOT EXISTS category (
+	categoryID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(128) NOT NULL)';
+if ($db->exec($query)===false){
+	die('Query failed(6):' . $db->errorInfo()[2]);
+}
+
+// Create table for reviews
+$query = 'CREATE TABLE IF NOT EXISTS reviews (
+	reviewsID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	receiverID INT NOT NULL,
+	writerID INT NOT NULL,
+	FOREIGN KEY (receiverID) REFERENCES users(userID),
+	FOREIGN KEY (writerID) REFERENCES users(userID))';
+if ($db->exec($query)===false){
+	die('Query failed(7):' . $db->errorInfo()[2]);
+}
+
+// Create table for messages
+$query = 'CREATE TABLE IF NOT EXISTS messages (
+	messageID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	receiverID INT NOT NULL,
+	writerID INT NOT NULL,
+	message TEXT NOT NULL,
+	date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+	itemID INT NOT  NULL,
+	status BOOLEAN DEFAULT 0,
+	FOREIGN KEY (itemID) REFERENCES items(itemID))';
+if ($db->exec($query)===false){
+	die('Query failed(8):' . $db->errorInfo()[2]);
+}
+
+// Create table for rating
+$query = 'CREATE TABLE IF NOT EXISTS rating (
+	userID INT NOT NULL,
+	up INT NOT NULL DEFAULT 0,
+	down INT NOT NULL DEFAULT 0,
+	FOREIGN KEY (userID) REFERENCES users(userID))';
+if ($db->exec($query)===false){
+	die('Query failed(9):' . $db->errorInfo()[2]);
+}
+
+// Create table for images
+$query = 'CREATE TABLE IF NOT EXISTS images (
+	itemID INT NOT NULL,
+	imgPath VARCHAR(256),
+	FOREIGN KEY (itemID) REFERENCES items(itemID))';
+if ($db->exec($query)===false){
+	die('Query failed(9):' . $db->errorInfo()[2]);
 }
