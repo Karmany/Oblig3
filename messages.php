d7ec1ad595c60ce84c4b154bb1bd03e1456b8da1
Karmany <haakonhorvei@hotmail.com>
2017-06-01 13:02:56 +0200

    Message update

--- messages.php
+++ messages.php
@@ -7,32 +7,35 @@ $stmnt = $db->prepare($sql);
 $stmnt->execute(array());
 $result = $stmnt->fetchAll(PDO::FETCH_OBJ);
 
-echo "<pre>";
-print_r ($result);
+
 foreach ($result as $row)
    {
-      echo $row->conversationID;
+      $convID = $row->conversationID;
+      // Get messages from database
+      $sql = "SELECT messageID, writerID, message FROM messages WHERE conversationID = '$convID'";
+      $stmnt = $db->prepare($sql);
+      $stmnt->execute(array());
+      $result = $stmnt->fetchAll(PDO::FETCH_OBJ);
+
+      echo "<hr><br><br> <h2>ConversationID =" . $convID . "</h2>";
+
+      // Print conversation
+      foreach ($result as $row)
+         {
+            echo "<pre>";
+            if ($row->writerID == $user_id) {
+               echo "<div class='col-sm-12 text-right'><p class='messageRight'>" . $row->message . " <b>:Written by you</b> " . "</p></div><br>";
+               //echo "Written by you: " . $row->message . "\n";
+            } else {
+               echo "<div class='col-sm-12'><p class='messageLeft'>" . "<b>Written by someone else: </b>" . $row->message . "</p></div><br>";
+               //echo "Written by someone else: " . $row->message . "\n";
+            }
+            echo "</pre>";
+         }
    }
 echo "</pre>";
 
 
-// Get messages from database
-$sql = "SELECT messageID, writerID, message FROM messages WHERE conversationID = 1";
-$stmnt = $db->prepare($sql);
-$stmnt->execute(array());
-$result = $stmnt->fetchAll(PDO::FETCH_OBJ);
 
-
-// Print conversation
-foreach ($result as $row)
-   {
-      if ($row->writerID == $user_id) {
-         echo "<div class='col-sm-12 text-right'><p class='messageRight'>" . $row->message . " <b>:Written by you</b> " . "</p></div><br>";
-         //echo "Written by you: " . $row->message . "\n";
-      } else {
-         echo "<div class='col-sm-12'><p class='messageLeft'>" . "<b>Written by someone else: </b>" . $row->message . "</p></div><br>";
-         //echo "Written by someone else: " . $row->message . "\n";
-      }
-   }
 
  ?>
